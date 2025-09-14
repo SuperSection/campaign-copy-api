aad058724599aec1ec69c945a4413592f428ad16
Soumo Sarkar <soumosarkar297@gmail.com>
2025-09-14 15:46:03 +0530

    Setup SQLite databse using SQLModel ORM

--- main.py
+++ main.py
@@ -1,26 +1,72 @@
-from datetime import datetime
+from contextlib import asynccontextmanager
+from datetime import datetime, timezone
 from random import randint
-from typing import Any
+from typing import Annotated, Any
 
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import Depends, FastAPI, HTTPException, Response
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-app = FastAPI(root_path="/api/v1")
+
+class Campaign(SQLModel, table=True):
+    campaign_id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    due_date: datetime | None = Field(default=None, index=True)
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc), nullable=True, index=True
+    )
 
 
-data: Any = [
-    {
-        "campaign_id": 1,
-        "name": "Summer Launch",
-        "due_date": datetime.now(),
-        "created_at": datetime.now(),
-    },
-    {
-        "campaign_id": 2,
-        "name": "Black Friday",
-        "due_date": datetime.now(),
-        "created_at": datetime.now(),
-    },
-]
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    with Session(engine) as session:
+        if not session.exec(select(Campaign)).first():
+            session.add_all(
+                [
+                    Campaign(name="Summer Launch", due_date=datetime.now()),
+                    Campaign(name="Black Friday", due_date=datetime.now()),
+                ]
+            )
+            session.commit()
+    yield
+
+
+app = FastAPI(root_path="/api/v1", lifespan=lifespan)
+
+
+# data: Any = [
+#     {
+#         "campaign_id": 1,
+#         "name": "Summer Launch",
+#         "due_date": datetime.now(),
+#         "created_at": datetime.now(),
+#     },
+#     {
+#         "campaign_id": 2,
+#         "name": "Black Friday",
+#         "due_date": datetime.now(),
+#         "created_at": datetime.now(),
+#     },
+# ]
 
 
 @app.get("/")
